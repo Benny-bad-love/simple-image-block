861575b720a60b8dd831391b26902d31f2c857ff
Ben <ben@pixeld.com.au>
2026-05-21 11:28:27 +1000

    Added override support for WordPress 7.0

--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Contributors:      The WordPress Contributors
 Tags:              block, image, gutenberg, media, patterns, overrides
 Tested up to:      6.9.4
-Stable tag:        0.2.3
+Stable tag:        0.2.4
 License:           GPL-2.0-or-later
 License URI:       https://www.gnu.org/licenses/gpl-2.0.html
 
@@ -82,6 +82,8 @@ Yes! The plugin will check for updates from the GitHub repository automatically.
 4. Pattern overrides in action with synced patterns
 
 == Changelog ==
+= 0.2.4 =
+* Added support for block overrides in WordPress 7.0+
 
 = 0.2.3 =
 * Removed package.json and package-lock.json from the plugin
--- simple-image-block.php
+++ simple-image-block.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name:       Simple Image Block
- * Description:       Super simple image block, no wrapper. Now with Pattern Overrides support for WordPress 6.6+!
+ * Description:       Super simple image block, no wrapper. Includes Pattern Overrides support for WordPress 7.0+.
  * Requires at least: 6.6
  * Requires PHP:      7.2
- * Version:           0.2.3
+ * Version:           0.2.4
  * Author:            Ben Grave
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
@@ -42,3 +42,27 @@ function create_block_simple_image_block_init() {
 	register_block_type( __DIR__ . '/build' );
 }
 add_action( 'init', 'create_block_simple_image_block_init' );
+
+/**
+ * Opt the block's image attributes into Block Bindings.
+ *
+ * WordPress uses this allow-list for sources such as core/pattern-overrides, so
+ * these attributes can be overridden inside synced pattern instances.
+ *
+ * @param string[] $supported_attributes Supported attribute names.
+ * @return string[] Supported attribute names.
+ */
+function create_block_simple_image_block_supported_binding_attributes( $supported_attributes ) {
+	return array_values(
+		array_unique(
+			array_merge(
+				$supported_attributes,
+				array( 'url', 'alt', 'title' )
+			)
+		)
+	);
+}
+add_filter(
+	'block_bindings_supported_attributes_create-block/simple-image-block',
+	'create_block_simple_image_block_supported_binding_attributes'
+);
